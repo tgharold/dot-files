4c14727229eb9ae72829f275fa2a76325c578b6b
Thomas Harold <tgharold@gmail.com>
2019-02-26 08:48:54 -0500

    add source URL

--- git/PrettyOnlineLog.sh
+++ git/PrettyOnlineLog.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 # Sets up a pretty online git log format, accessible with "git l"
+# https://ma.ttias.be/pretty-git-log-in-one-line/
+# Note: This sets values in your ~/.gitconfig file.
 
 git config --global alias.l "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
  
